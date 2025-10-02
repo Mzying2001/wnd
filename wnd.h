2c2e7201c1c06d25eaa2b500ee8ab8126c676d6d
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-10-02 18:34:33 +0800

    Update wnd.h

--- wnd.h
+++ wnd.h
@@ -2,355 +2,289 @@
 #define _WND_H_
 
 #include <windows.h>
-#include <cassert>
+#include <string>
 
-#define PROPNAME_WNDPTR TEXT("__WNDPTR")
+#if !(defined(UNICODE) || defined(_UNICODE))
+#define WND_USE_ANSI_WINDPROC
+#endif
 
-template <typename TDerived>
-struct Wnd {
-    HWND hWnd;
-    WNDPROC oldproc;
-
-    explicit Wnd(HWND hWnd)
-        : hWnd(hWnd)
-    {
-        assert(hWnd != NULL);
-
-        ::SetProp(hWnd, PROPNAME_WNDPTR,
-                  reinterpret_cast<HANDLE>(this));
-
-        oldproc = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(
-            hWnd, GWLP_WNDPROC,
-            reinterpret_cast<LONG_PTR>((WNDPROC)[](HWND h, UINT m, WPARAM w, LPARAM l)->LRESULT {
-                Wnd *pWnd = reinterpret_cast<Wnd *>(::GetProp(h, PROPNAME_WNDPTR));
-                if (pWnd) {
-                    LRESULT result = 0;
-                    auto *pDerived = static_cast<TDerived *>(pWnd);
-                    return pDerived->WndProc(m, w, l, result) ? result : pDerived->DefWindowProc(m, w, l);
-                } else
-                    return ::DefWindowProc(h, m, w, l);
-            })));
-    }
-    ~Wnd()
-    {
-        if (hWnd != NULL) {
-            DestroyWindow();
-        }
-    }
-
-    Wnd(const Wnd &)            = delete;
-    Wnd &operator=(const Wnd &) = delete;
-
-    Wnd(Wnd &&other)
-    {
-        assert(&other != this);
-
-        hWnd    = other.hWnd;
-        oldproc = other.oldproc;
-
-        ::SetProp(other.hWnd, PROPNAME_WNDPTR,
-                  reinterpret_cast<HANDLE>(this));
-
-        other.hWnd    = NULL;
-        other.oldproc = nullptr;
-    }
-    Wnd &operator=(Wnd &&other)
-    {
-        if (&other == this) {
-            return *this;
-        }
-        if (hWnd != NULL) {
-            DestroyWindow();
-        }
-
-        hWnd    = other.hWnd;
-        oldproc = other.oldproc;
-
-        ::SetProp(other.hWnd, PROPNAME_WNDPTR,
-                  reinterpret_cast<HANDLE>(this));
-
-        other.hWnd    = NULL;
-        other.oldproc = nullptr;
-        return *this;
-    }
-    bool operator==(const Wnd &other) const
-    {
-        return hWnd == other.hWnd;
-    }
-    bool operator!=(const Wnd &other) const
-    {
-        return hWnd != other.hWnd;
-    }
-    explicit operator HWND() const
-    {
-        return hWnd;
-    }
-
-    LRESULT DefWindowProcA(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        WNDPROC wndproc = oldproc ? oldproc : ::DefWindowProcA;
-        return wndproc(hWnd, msg, wParam, lParam);
-    }
-    LRESULT DefWindowProcW(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        WNDPROC wndproc = oldproc ? oldproc : ::DefWindowProcW;
-        return wndproc(hWnd, msg, wParam, lParam);
-    }
-    LRESULT SendMessageA(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        return ::SendMessageA(hWnd, msg, wParam, lParam);
-    }
-    LRESULT SendMessageW(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        return ::SendMessageW(hWnd, msg, wParam, lParam);
-    }
-    BOOL PostMessageA(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        return ::PostMessageA(hWnd, msg, wParam, lParam);
-    }
-    BOOL PostMessageW(UINT msg, WPARAM wParam, LPARAM lParam) const
-    {
-        return ::PostMessageW(hWnd, msg, wParam, lParam);
-    }
-    LONG GetWindowLongA(int nIndex) const
-    {
-        return ::GetWindowLongA(hWnd, nIndex);
-    }
-    LONG GetWindowLongW(int nIndex) const
-    {
-        return ::GetWindowLongW(hWnd, nIndex);
-    }
-    LONG SetWindowLongA(int nIndex, LONG dwNewLong) const
-    {
-        return ::SetWindowLongA(hWnd, nIndex, dwNewLong);
-    }
-    LONG SetWindowLongW(int nIndex, LONG dwNewLong) const
-    {
-        return ::SetWindowLongW(hWnd, nIndex, dwNewLong);
-    }
-    LONG_PTR GetWindowLongPtrA(int nIndex) const
-    {
-        return ::GetWindowLongPtrA(hWnd, nIndex);
-    }
-    LONG_PTR GetWindowLongPtrW(int nIndex) const
-    {
-        return ::GetWindowLongPtrW(hWnd, nIndex);
-    }
-    LONG_PTR SetWindowLongPtrA(int nIndex, LONG_PTR dwNewLong) const
-    {
-        return ::SetWindowLongPtrA(hWnd, nIndex, dwNewLong);
-    }
-    LONG_PTR SetWindowLongPtrW(int nIndex, LONG_PTR dwNewLong) const
-    {
-        return ::SetWindowLongPtrW(hWnd, nIndex, dwNewLong);
-    }
-    HANDLE GetPropA(LPCSTR lpString) const
-    {
-        return ::GetPropA(hWnd, lpString);
-    }
-    HANDLE GetPropW(LPCWSTR lpString) const
-    {
-        return ::GetPropW(hWnd, lpString);
-    }
-    BOOL SetPropA(LPCSTR lpString, HANDLE hData) const
-    {
-        return ::SetPropA(hWnd, lpString, hData);
-    }
-    BOOL SetPropW(LPCWSTR lpString, HANDLE hData) const
-    {
-        return ::SetPropW(hWnd, lpString, hData);
-    }
-    BOOL SetWindowTextA(LPCSTR lpString) const
-    {
-        return ::SetWindowTextA(hWnd, lpString);
-    }
-    BOOL SetWindowTextW(LPCWSTR lpString) const
-    {
-        return ::SetWindowTextW(hWnd, lpString);
-    }
-    int GetWindowTextLengthA() const
-    {
-        return ::GetWindowTextLengthA(hWnd);
-    }
-    int GetWindowTextLengthW() const
-    {
-        return ::GetWindowTextLengthW(hWnd);
-    }
-    int GetWindowTextA(LPSTR lpString, int nMaxCount) const
-    {
-        return ::GetWindowTextA(hWnd, lpString, nMaxCount);
-    }
-    int GetWindowTextW(LPWSTR lpString, int nMaxCount) const
-    {
-        return ::GetWindowTextW(hWnd, lpString, nMaxCount);
-    }
-    HWND SetParent(HWND hWndNewParent) const
-    {
-        return ::SetParent(hWnd, hWndNewParent);
-    }
-    HWND GetParent() const
-    {
-        return ::GetParent(hWnd);
-    }
-    BOOL ShowWindow(int nCmdShow) const
-    {
-        return ::ShowWindow(hWnd, nCmdShow);
-    }
-    BOOL DestroyWindow() const
-    {
-        return ::DestroyWindow(hWnd);
-    }
-    BOOL EnableWindow(BOOL bEnable) const
-    {
-        return ::EnableWindow(hWnd, bEnable);
-    }
-    BOOL UpdateWindow() const
-    {
-        return ::UpdateWindow(hWnd);
-    }
-    BOOL ValidateRect(const RECT *lpRect) const
-    {
-        return ::ValidateRect(hWnd, lpRect);
-    }
-    BOOL InvalidateRect(const RECT *lpRect, BOOL bErase) const
-    {
-        return ::InvalidateRect(hWnd, lpRect, bErase);
-    }
-    BOOL MoveWindow(int X, int Y, int nWidth, int nHeight, BOOL bRepaint) const
-    {
-        return ::MoveWindow(hWnd, X, Y, nWidth, nHeight, bRepaint);
-    }
-    BOOL SetWindowPos(HWND hWndInsertAfter, int X, int Y, int cx, int cy, UINT uFlags) const
-    {
-        return ::SetWindowPos(hWnd, hWndInsertAfter, X, Y, cx, cy, uFlags);
-    }
-    BOOL BringWindowToTop() const
-    {
-        return ::BringWindowToTop(hWnd);
-    }
-    BOOL IsWindowVisible() const
-    {
-        return ::IsWindowVisible(hWnd);
-    }
-    BOOL GetWindowRect(LPRECT lpRect) const
-    {
-        return ::GetWindowRect(hWnd, lpRect);
-    }
-    BOOL GetClientRect(LPRECT lpRect) const
-    {
-        return ::GetClientRect(hWnd, lpRect);
-    }
-    BOOL ScreenToClient(LPPOINT lpPoint) const
-    {
-        return ::ScreenToClient(hWnd, lpPoint);
-    }
-    BOOL ClientToScreen(LPPOINT lpPoint) const
-    {
-        return ::ClientToScreen(hWnd, lpPoint);
-    }
-    HWND GetDlgItem(int nIDDlgItem) const
-    {
-        return ::GetDlgItem(hWnd, nIDDlgItem);
-    }
-    int GetDlgCtrlID() const
-    {
-        return ::GetDlgCtrlID(hWnd);
-    }
-    BOOL FlashWindow(BOOL bInvert) const
-    {
-        return ::FlashWindow(hWnd, bInvert);
-    }
-    HDC BeginPaint(LPPAINTSTRUCT lpPaint) const
-    {
-        return ::BeginPaint(hWnd, lpPaint);
-    }
-    BOOL EndPaint(const PAINTSTRUCT *lpPaint) const
-    {
-        return ::EndPaint(hWnd, lpPaint);
-    }
-    HDC GetDC() const
-    {
-        return ::GetDC(hWnd);
-    }
-    HDC GetDCEx(HRGN hrgnClip, DWORD flags) const
-    {
-        return ::GetDCEx(hWnd, hrgnClip, flags);
-    }
-    int ReleaseDC(HDC hDC) const
-    {
-        return ::ReleaseDC(hWnd, hDC);
-    }
-    int GetUpdateRgn(HRGN hRgn, BOOL bErase) const
-    {
-        return ::GetUpdateRgn(hWnd, hRgn, bErase);
-    }
-    BOOL GetUpdateRect(LPRECT lpRect, BOOL bErase) const
-    {
-        return ::GetUpdateRect(hWnd, lpRect, bErase);
-    }
-    HWND SetCapture() const
-    {
-        return ::SetCapture(hWnd);
-    }
-    BOOL ReleaseCapture() const
-    {
-        return ::ReleaseCapture();
-    }
-    HWND GetFocus() const
-    {
-        return ::GetFocus();
-    }
-    HWND SetFocus() const
-    {
-        return ::SetFocus(hWnd);
-    }
-    HMENU GetSystemMenu(BOOL bRevert) const
-    {
-        return ::GetSystemMenu(hWnd, bRevert);
-    }
-    void DragAcceptFiles(BOOL fAccept) const
-    {
-        ::DragAcceptFiles(hWnd, fAccept);
-    }
-    BOOL SetLayeredWindowAttributes(COLORREF crKey, BYTE bAlpha, DWORD dwFlags) const
-    {
-        return ::SetLayeredWindowAttributes(hWnd, crKey, bAlpha, dwFlags);
-    }
-
-    // bool WndProc(UINT msg, WPARAM wParam, LPARAM lParam, LRESULT &result)
-    // {
-    //     return false;
-    // }
+struct Msg
+{
+    UINT    uMsg;
+    WPARAM  wParam;
+    LPARAM  lParam;
 };
 
-#define DECLARE_CONTROL(_class, _lpClassName, _dwStyle, _dwExStyle)                               \
-    class _class : public Wnd<_class>                                                             \
-    {                                                                                             \
-        using TBase = Wnd<_class>;                                                                \
-                                                                                                  \
-    public:                                                                                       \
-        _class(HWND hParent, int id, LPCTSTR lpWindowName = TEXT(#_class),                        \
-               int x = 0, int y = 0, int cx = 100, int cy = 100, LPVOID lpParam = NULL)           \
-            : TBase(CreateWindowEx(_dwExStyle, _lpClassName, lpWindowName, _dwStyle,              \
-                                   x, y, cx, cy,                                                  \
-                                   hParent, (HMENU)(LONG_PTR)id, GetModuleHandle(NULL), lpParam)) \
-        {                                                                                         \
-        }                                                                                         \
-        virtual ~_class()                                                                         \
-        {                                                                                         \
-        }                                                                                         \
-        _class(_class &&other) : TBase(std::move(other))                                          \
-        {                                                                                         \
-        }                                                                                         \
-        _class &operator=(_class &&other)                                                         \
-        {                                                                                         \
-            TBase::operator=(std::move(other));                                                   \
-            return *this;                                                                         \
-        }                                                                                         \
-        virtual bool WndProc(UINT msg, WPARAM wParam, LPARAM lParam, LRESULT &result)             \
-        {                                                                                         \
-            return false;                                                                         \
-        }                                                                                         \
+template <typename TDerived>
+class Wnd
+{
+    HWND _hWnd;
+    bool _destroyed;
+    WNDPROC _defWndProc;
+
+private:
+    static constexpr WCHAR PROP_THIS[] = L"__Wnd_This_Ptr";
+
+    struct _CreateParamWrapper
+    {
+        Wnd* pThis;
+        LPVOID lpParam;
+    };
+
+private:
+    static Wnd* GetThisFromHandle(HWND hWnd) noexcept
+    {
+        return reinterpret_cast<Wnd*>(::GetPropW(hWnd, PROP_THIS));
     }
 
-#endif // _WND_H_
+    static bool BindThisToHandle(HWND hWnd, Wnd* pThis) noexcept
+    {
+        return ::SetPropW(hWnd, PROP_THIS, reinterpret_cast<HANDLE>(pThis)) != 0;
+    }
+
+    static LRESULT CALLBACK StaticWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        Wnd* pThis = GetThisFromHandle(hWnd);
+
+        if (pThis == nullptr &&
+            (uMsg == WM_NCCREATE || uMsg == WM_CREATE))
+        {
+            CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+            _CreateParamWrapper* pWrapper = reinterpret_cast<_CreateParamWrapper*>(pCreate->lpCreateParams);
+
+            if (pWrapper != nullptr) {
+                pThis = pWrapper->pThis;
+                pThis->_hWnd = hWnd;
+                BindThisToHandle(hWnd, pThis);
+                pCreate->lpCreateParams = pWrapper->lpParam;
+            }
+        }
+
+        if (pThis != nullptr)
+        {
+            Msg msg{ uMsg, wParam, lParam };
+            LRESULT result = 0;
+
+            if (!pThis->WndProc(msg, result)) {
+                result = pThis->DefWndProc(msg);
+            }
+            if (uMsg == WM_NCDESTROY) {
+                pThis->_destroyed = true;
+            }
+            return result;
+        }
+
+#if defined(WND_USE_ANSI_WINDPROC)
+        return ::DefWindowProcA(hWnd, uMsg, wParam, lParam);
+#else
+        return ::DefWindowProcW(hWnd, uMsg, wParam, lParam);
+#endif
+    }
+
+protected:
+    Wnd() noexcept
+        : _hWnd(NULL), _destroyed(false), _defWndProc(NULL)
+    {
+    }
+
+#if defined(WND_USE_ANSI_WINDPROC)
+    bool Create(
+        DWORD dwExStyle,
+        LPCSTR lpClassName,
+        LPCSTR lpWindowName,
+        DWORD dwStyle,
+        int X,
+        int Y,
+        int nWidth,
+        int nHeight,
+        HWND hWndParent,
+        HMENU hMenu,
+        HINSTANCE hInstance,
+        LPVOID lpParam) noexcept
+    {
+        if (_hWnd != NULL || lpClassName == nullptr) {
+            return false;
+        }
+
+        WNDCLASSEXA wc = { 0 };
+        wc.cbSize = sizeof(wc);
+
+        if (!::GetClassInfoExA(hInstance, lpClassName, &wc)) {
+            return false;
+        }
+        else {
+            _defWndProc = wc.lpfnWndProc;
+        }
+
+        std::string className = lpClassName;
+        className += "_Wnd_Class";
+
+        if (!::GetClassInfoExA(hInstance, className.c_str(), &wc))
+        {
+            wc.lpfnWndProc = StaticWndProc;
+            wc.lpszClassName = className.c_str();
+
+            if (!::RegisterClassExA(&wc)) {
+                return false;
+            }
+        }
+
+        _CreateParamWrapper createParam = { this, lpParam };
+
+        _hWnd = ::CreateWindowExA(
+            dwExStyle,
+            className.c_str(),
+            lpWindowName,
+            dwStyle,
+            X,
+            Y,
+            nWidth,
+            nHeight,
+            hWndParent,
+            hMenu,
+            hInstance,
+            &createParam);
+
+        return _hWnd != NULL;
+    }
+#else
+    bool Create(
+        DWORD dwExStyle,
+        LPCWSTR lpClassName,
+        LPCWSTR lpWindowName,
+        DWORD dwStyle,
+        int X,
+        int Y,
+        int nWidth,
+        int nHeight,
+        HWND hWndParent,
+        HMENU hMenu,
+        HINSTANCE hInstance,
+        LPVOID lpParam) noexcept
+    {
+        if (_hWnd != NULL || lpClassName == nullptr) {
+            return false;
+        }
+
+        WNDCLASSEXW wc = { 0 };
+        wc.cbSize = sizeof(wc);
+
+        if (!::GetClassInfoExW(hInstance, lpClassName, &wc)) {
+            return false;
+        }
+        else {
+            _defWndProc = wc.lpfnWndProc;
+        }
+
+        std::wstring className = lpClassName;
+        className += L"_Wnd_Class";
+
+        if (!::GetClassInfoExW(hInstance, className.c_str(), &wc))
+        {
+            wc.lpfnWndProc = StaticWndProc;
+            wc.lpszClassName = className.c_str();
+
+            if (!::RegisterClassExW(&wc)) {
+                return false;
+            }
+        }
+
+        _CreateParamWrapper createParam = { this, lpParam };
+
+        _hWnd = ::CreateWindowExW(
+            dwExStyle,
+            className.c_str(),
+            lpWindowName,
+            dwStyle,
+            X,
+            Y,
+            nWidth,
+            nHeight,
+            hWndParent,
+            hMenu,
+            hInstance,
+            &createParam);
+
+        return _hWnd != NULL;
+    }
+#endif
+
+    LRESULT DefWndProc(const Msg& msg)
+    {
+#if defined(WND_USE_ANSI_WINDPROC)
+        return _defWndProc != nullptr
+            ? ::CallWindowProcA(_defWndProc, _hWnd, msg.uMsg, msg.wParam, msg.lParam)
+            : ::DefWindowProcA(_hWnd, msg.uMsg, msg.wParam, msg.lParam);
+#else
+        return _defWndProc != nullptr
+            ? ::CallWindowProcW(_defWndProc, _hWnd, msg.uMsg, msg.wParam, msg.lParam)
+            : ::DefWindowProcW(_hWnd, msg.uMsg, msg.wParam, msg.lParam);
+#endif
+    }
+
+    bool WndProc(const Msg& msg, LRESULT& result)
+    {
+        static_assert(&Wnd::WndProc != &TDerived::WndProc,
+            "Derived class must implement WndProc method.");
+        return static_cast<TDerived*>(this)->TDerived::WndProc(msg, result);
+    }
+
+public:
+    Wnd(const Wnd&) = delete;
+    Wnd& operator=(const Wnd&) = delete;
+    Wnd& operator=(Wnd&&) = delete;
+
+    Wnd(Wnd&& other) noexcept
+        : _hWnd(other._hWnd), _destroyed(other._destroyed), _defWndProc(other._defWndProc)
+    {
+        if (!_destroyed && _hWnd != NULL) {
+            BindThisToHandle(_hWnd, this);
+        }
+        other._hWnd = NULL;
+        other._destroyed = true;
+        other._defWndProc = NULL;
+    }
+
+    ~Wnd() noexcept
+    {
+        if (!_destroyed && _hWnd != NULL)
+        {
+            ::RemovePropW(_hWnd, PROP_THIS);
+            ::DestroyWindow(_hWnd);
+            _hWnd = NULL;
+            _destroyed = true;
+        }
+    }
+
+    HWND Handle() const noexcept
+    {
+        return _hWnd;
+    }
+
+    bool IsDestroyed() const noexcept
+    {
+        return _destroyed;
+    }
+
+    LRESULT SendMessageA(UINT uMsg, WPARAM wParam = 0, LPARAM lParam = 0) noexcept
+    {
+        return ::SendMessageA(_hWnd, uMsg, wParam, lParam);
+    }
+
+    LRESULT SendMessageW(UINT uMsg, WPARAM wParam = 0, LPARAM lParam = 0) noexcept
+    {
+        return ::SendMessageW(_hWnd, uMsg, wParam, lParam);
+    }
+
+    LRESULT PostMessageA(UINT uMsg, WPARAM wParam = 0, LPARAM lParam = 0) noexcept
+    {
+        return ::PostMessageA(_hWnd, uMsg, wParam, lParam);
+    }
+
+    LRESULT PostMessageW(UINT uMsg, WPARAM wParam = 0, LPARAM lParam = 0) noexcept
+    {
+        return ::PostMessageW(_hWnd, uMsg, wParam, lParam);
+    }
+};
+
+#endif // !_WND_H_
